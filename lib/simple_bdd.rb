a1f2e7c3b9f7672d03bd1da54cdf91f88e89fa31
Dan Finnie <dan@danfinnie.com>
2013-06-16 12:34:32 -0400

    Get rid of Core and NamedNouns modules

--- lib/simple_bdd.rb
+++ lib/simple_bdd.rb
@@ -3,10 +3,49 @@ require "simple_bdd/core"
 require "simple_bdd/named_nouns"
 
 module SimpleBdd
-    include SimpleBdd::Core
-    include SimpleBdd::NamedNouns
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
 
-    def self.included(base)
-        base.extend(ClassMethods)
+  %w[Given When Then And Also].each do |method|
+    define_method(method) do |message|
+      begin
+        send methodize(message)
+      rescue NoMethodError => error
+        message_does_not_exist(
+          methodize(message), error
+        )
+      end
     end
+
+    alias_method method.downcase, method
+  end
+
+  PRESERVED_CHARS = '\\w'
+  CONVERTED_CHARS = Regexp.escape(' /')
+
+  def methodize(message)
+    message
+      .downcase
+      .gsub(/[^#{PRESERVED_CHARS}#{CONVERTED_CHARS}]/, "")
+      .gsub(/[#{CONVERTED_CHARS}]+/, "_")
+  end
+
+  if defined?(::RSpec)
+    def message_does_not_exist(message, error)
+      pending(message)
+    end
+  else
+    def message_does_not_exist(message, error)
+      raise error
+    end
+  end
+
+  module ClassMethods
+    def let_named!(instance_name, class_name, &constructor)
+      let!(instance_name, &constructor)
+      @named_nouns ||= {}
+      @named_nouns[instance_name.to_sym] = class_name.to_s
+    end
+  end
 end
